23492eba6a1348608d800682cd1d6b0cf4aed19a
saeel <saeel_parsekar@persistent.co.in>
2016-05-02 17:10:27 +0530

    Bar Chart and Word Cloud

--- server.js
+++ server.js
@@ -53,6 +53,43 @@ dbObject.collection('socialfact').aggregate([
 
 }
 
+function getWords(responseObj){
+dbObject.collection('wordcount').aggregate([
+   { $match: { count: { $gte: 100 } } },
+    { $group: {_id: "$word", counter: {$first: "$count"}} },   
+    {$limit:500},
+]).toArray(function(err, docs){
+    	
+	if ( err ) throw err;
+	var c = 0;
+	var dataset = [];
+	for ( index in docs){		
+      var doc = docs[index];
+     c = c+1;
+        
+       var randNumber =  Math.floor(Math.random() * 8) + 1;
+        var relevancy =  Math.floor(Math.random() * 6) + 1;
+      
+        if(relevancy > randNumber){
+            var temp = randNumber;
+            randNumber = relevancy;
+            relevancy = temp;
+        }
+     
+    if(doc['_id'] != null && doc['_id'] != ""){    
+     var outString = doc['_id'].replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');          
+     dataset.push({"name" : outString,"years" :randNumber,"relevancy" :relevancy});
+    }
+    }
+  
+
+   var response = dataset;
+	
+	//console.log(dataset);
+    responseObj.json(response);   
+  });    
+}
+
 
 function getLocation(responseObj) {
 dbObject.collection('socialfact').aggregate([
@@ -135,33 +172,130 @@ dbObject.collection('socialfact').aggregate([
     responseObj.json(response);
   });
 }
+
+//bar graph data
+function getBardata(responseObj) {
+dbObject.collection('socialfact').aggregate([
+    { $match: { count: { $gte: 10 } } },
+    { $group: {_id: "$dataplace", counter: {$first: "$count"}} },   
+    {$limit:5},
+]).toArray(function(err, docs){
+//form the data in required format for stacked chart
+if ( err ) throw err;
+	var dataset = [];
+	var child = [];
+	var editeditems = [];
+    var c = 0;    
+for ( index in docs){		
+      var doc = docs[index];
+        c = c+1;	
+		if( doc['_id'] != 'unknown') {
+		child.push({"label" : doc['_id'],"value" : doc['counter']});
+        }
+}
+
+	editeditems.push({
+		key: "Locationwise Tweet Count",
+		"values": child
+		});  
+	
+//send the response
+var response = editeditems;
+    responseObj.json(response);
+});
+
+}	
  
- 
-function getGenderLocation(responseObj) {
-/*dbObject.collection('socialdata').aggregate([
-    { "$group": {
+//
+
+
+function getStackeddata(responseObj) {
+
+dbObject.collection('socialfact').aggregate([
+    
+    /*{ "$group": {
         "_id": {
-            
+            "date": "$post_time",
             "gender": "$gender",
-			"location": "$dataPlace",
-        },
-        "totalCount": { "$sum": 1 }
+            "tweetcount": "$count"
+           
+        }    
     }},
     { "$group": {
         "_id": "$_id.gender",
-        "Genders": { 
-            "$push": { 
-                "gender": "$_id.location",
-                "count": "$totalCount",
+        "DataSet": { 
+            "$addToSet": { 
+                "date": "$_id.date",
+                "tweetcount":"$_id.tweetcount",
+               
                
             },
         },
-        "count": { "$sum": "$totalCount" }
+       
     }},
-    
-    
-    { "$sort": { "count": -1 } },
-])*/dbObject.collection('socialdata').aggregate([
+  { "$sort": { "count": -1 } }, 
+])*/
+
+
+   
+    { "$group": {
+        "_id": {
+            "date": "$post_time",
+            "gender": "$gender",
+            "tweetcount": "$count"
+           
+        }    
+    }},
+    { "$group": {
+        "_id": "$_id.gender",
+        "DataSet": { 
+            "$addToSet": { 
+                "date": "$_id.date",
+                "tweetcount":"$_id.tweetcount",
+               
+               
+            },
+        },
+       
+    }},
+  { "$sort": { "count": -1 } }, 
+]).toArray(function(err, docs){
+//form the data in required format for stacked chart
+if ( err ) throw err;
+	var dataset = [];
+	var child = [];
+	var editeditems = [];
+for (index in docs){
+	var settings = [];
+	var doc = docs[index];
+	for ( C in doc['DataSet']){
+		var d = doc['DataSet'][C];
+		var childrens=[];
+		var dateTime = d['date'];
+		settings.push([dateTime,d['tweetcount']]);
+							
+	}			
+	if(doc['_id'] != "U") {
+	editeditems.push({		
+		"key": doc['_id'],
+		"values": settings
+		});  
+	}
+}
+
+
+
+
+
+//send the response
+var response = editeditems;
+    responseObj.json(response);
+});
+
+}	
+ 
+function getGenderLocation(responseObj) {
+dbObject.collection('socialdata').aggregate([
     { "$group": {
         "_id": {
             "location": "$dataPlace",
@@ -188,12 +322,10 @@ function getGenderLocation(responseObj) {
     
     
     { "$sort": { "count": -1 } },
-]).toArray(function(err, docs){
-    	
+]).toArray(function(err, docs){   	
 	if ( err ) throw err;
 	var dataset = [];
 	var child = [];
-
 	var editeditems = [];
 for (index in docs){
 	var settings = [];
@@ -201,8 +333,7 @@ for (index in docs){
 	for ( C in doc['Locations']){
 		var d = doc['Locations'][C];
 		var childrens=[];
-		for(E in d['names']){
-			
+		for(E in d['names']){			
 		 var f = d['names'][E];
 			childrens.push({
 				"name": f['username'],
@@ -216,8 +347,7 @@ for (index in docs){
 					 });
 	}			
 	if(doc['_id'] != "U") {
-	editeditems.push({
-		
+	editeditems.push({		
 		"name": doc['_id'],
 		"children": settings
 		});  
@@ -264,6 +394,28 @@ app.get("/getCampaign", function(req, res){
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   getCampaign(res); 
 });
+
+app.get("/getStackeddata", function(req, res){
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');  
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  getStackeddata(res); 
+});
+
+app.get("/getBardata", function(req, res){
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');  
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  getBardata(res); 
+});
+
+app.get("/getWords", function(req, res){
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');  
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  getWords(res); 
+});
+
 app.listen("3300", function(){
   console.log('Server up: http://localhost:3300');
 });
